77e9bff9931a55c1c0d741983b296220d07546b1
jiwon020207 <ojiwon58@gmail.com>
2025-07-20 23:04:41 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
+# app/main.py
+
 from fastapi import FastAPI
-from app.routers import auth, faces, protections
+import app.routers.auth as auth
+import app.routers.faces as faces
+import app.routers.protections as protections
 
 app = FastAPI(title="SeeCure Backend")
-app.include_router(auth.router, prefix="/api/v1/auth")
-app.include_router(faces.router, prefix="/api/v1/faces")
-app.include_router(protections.router, prefix="/api/v1/protections")
+
+app.include_router(auth.router, prefix="/api/v1/auth", tags=["auth"])
+app.include_router(faces.router, prefix="/api/v1/faces", tags=["faces"])
+app.include_router(protections.router, prefix="/api/v1/protections", tags=["protections"])
 
 @app.get("/", tags=["health"])
 def health_check():
